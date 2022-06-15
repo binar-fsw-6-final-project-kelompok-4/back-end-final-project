6dbb4deee19c4125d67af2492ef3e8189de0dfdd
amosaugusto <amosaugusto@gmail.com>
2022-06-15 20:56:27 +0700

    fix user register

--- app/controllers/api/v1/userController.js
+++ app/controllers/api/v1/userController.js
@@ -1,42 +1,36 @@
 const bcrypt = require("bcryptjs");
 const {
-    User
+    users
 } = require("../../../models");
 
-const getByEmail = async (req, res, email) => {
-    return User.findOne({
-        where: {
-            email,
-        },
-    });
-};
+// const getByEmail = async (req, res, email) => {
+//     return users.findOne({
+//         where: {
+//             email,
+//         },
+//     });
+// };
 
 const createUser = async (req, res) => {
-    const existedUser = await getByEmail(req.body.email);
-    if (existedUser) {
-        return res.status(400).send({
-            message: "Email has been used",
-        });
-    }
+    // const existedUser = await getByEmail(req.body.email);
+    // if (existedUser) {
+    //     return res.status(400).send({
+    //         message: "Email has been used",
+    //     });
+    // }
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    User.create({
+    users.create({
             email: req.body.email,
             password: hashedPassword,
             username: req.body.username,
-            name: req.body.name,
-            address: req.body.address,
-            contact: req.body.contact,
-            city: req.body.string,
-            profileImg: req.body.profileImg,
-            roleId: 1,
             createdAt: new Date(),
             updatedAt: new Date(),
 
         })
-        .then((user) => {
+        .then((users) => {
             res.status(201).json({
                 status: "OK",
-                data: user,
+                data: users,
             });
         })
         .catch((err) => {
@@ -49,5 +43,5 @@ const createUser = async (req, res) => {
 
 module.exports = {
     createUser,
-    getByEmail
+    // getByEmail
 };
