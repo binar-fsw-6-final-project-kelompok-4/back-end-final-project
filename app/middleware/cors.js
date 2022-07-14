ac85881e69c52af9a89435a754ea1164a8bafc30
dhewan <dhewanagumm@gmail.com>
2022-07-14 11:35:05 +0700

    fix cors allow2

--- app/middleware/cors.js
+++ app/middleware/cors.js
@@ -1,8 +1,7 @@
-
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://fsw6-group4-staging.herokuapp.com');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
