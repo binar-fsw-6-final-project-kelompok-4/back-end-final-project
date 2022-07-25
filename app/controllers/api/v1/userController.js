aab7ef3fd76e12f668dfd20d21bd5acbd589a92c
amosaugusto <amosaugusto@gmail.com>
2022-07-25 10:31:18 +0700

    fix user login

--- app/controllers/api/v1/userController.js
+++ app/controllers/api/v1/userController.js
@@ -175,42 +175,43 @@ const login = async (req, res) => {
     try {
         const user = await users.findOne({
             where: {
-                email: req.body.email
-            }
-        })
+                email: req.body.email,
+            },
+        });
         if (!user) {
             res.status(404).send({
                 status: 404,
-                message: 'user not found!',
-            })
+                message: "user Not Found!",
+            });
         }
-
-        const isValidPassword = await bcrypt.compare(req.body.password, user.password)
-
+        const isValidPassword = await bcrypt.compare(
+            req.body.password,
+            // password ditabase
+            user.password
+        );
         if (!isValidPassword) {
             res.status(404).send({
                 status: 400,
-                message: 'Email and password not match!',
-            })
+                message: "Email and Password not match!",
+            });
         }
         const token = jwt.generateToken({
             email: user.email,
-            password: user.password
-        })
-        const secureuser = user.dataValues
-        delete secureuser.password
-
+            password: user.password,
+        });
+        const secureuser = user.dataValues;
+        delete secureuser.password;
+        res.header("token", token);
         res.status(200).send({
             status: 200,
-            message: 'Login success',
+            message: "user Found",
             data: {
+                secureuser,
                 token: token,
-                user: secureuser
-            }
-        })
+            },
+        });
     } catch (error) {
-        console.log(error);
-        res.status(500).send(error)
+        res.status(400).send(error);
     }
 }
 
